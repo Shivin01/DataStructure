93ff41f64800f346f3d1474ede2bf27b7b8745da
shivin01 <13720105+Shivin01@users.noreply.github.com>
2024-09-26 19:48:23 +0530

    Added more
    Signed-off-by: shivin01 <13720105+Shivin01@users.noreply.github.com>

---
+++ Greedy/GreedyC/FractionalKnapSack.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h> 
+
+bool cmp(pair<int, int> &a, pair<int, int> &b) {
+    double r1 = (double)a.second / (double)a.first;
+    double r2 = (double)b.second / (double)b.first;
+    return r1>r2;
+}
+
+double maximumValue (vector<pair<int, int>>& items, int n, int w)
+{
+    // Write your code here.
+    // ITEMS contains {weight, value} pairs.
+    
+    sort(items.begin(), items.end(), cmp);
+
+    double totalValue = 0.0;
+    int currentWeight = 0;
+
+    for(auto &item: items ) {
+        int itemWeight = item.first;
+        int itemValue = item.second;
+
+        if (currentWeight + itemWeight <= w) {
+            totalValue += itemValue;
+            currentWeight += itemWeight;
+        } else {
+            int remainCapacity = w - currentWeight;
+            totalValue += itemValue * ( (double)remainCapacity/itemWeight );
+            break;
+        }        
+    }
+
+    return totalValue;
+}
